e811cd074aa5f0306ca7cc7fcbaca9a46382762f
anwar <anwarcourri12@gmail.com>
2024-09-26 19:07:51 +0200

    Initial commit

---
+++ 3.1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Inserisci un numero: ");
+    scanf("%d",&num);
+    if(num%2==0) {
+        printf("Il numero è pari");
+    }
+    else{printf("Il numero è dispari");}
+
+    return 0;
+}
